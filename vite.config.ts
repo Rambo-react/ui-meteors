a2879b52d46256d594c2043924431e6ddac4dd4a
Rambo <sandstorm0819@gmail.com>
2024-10-22 13:47:14 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { resolve } from 'path'
+
+import { defineConfig } from 'vite'
+
+import { dependencies, devDependencies } from './package.json'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'index',
+      formats: ['es'],
+      name: 'ui-meteors',
+      // the proper extensions will be added
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [
+        ...Object.keys(dependencies),
+        ...Object.keys(devDependencies),
+        'react/jsx-runtime',
+      ],
+    },
+    sourcemap: true,
+    target: 'esnext',
+  },
+})
