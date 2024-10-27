bbd2e08b1422d5681825bd2085bcc2c0230cf051
Yan Turnt <bx6xbchosen1@gmail.com>
2024-10-27 18:40:58 +0500

    PROJ-13_tabs feat: add Tabs logic

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,5 +1,39 @@
+import { ComponentPropsWithoutRef } from 'react'
+
+import * as RadixTabs from '@radix-ui/react-tabs'
+import clsx from 'clsx'
+
 import s from './Tabs.module.scss'
 
-export const Tabs = () => {
-  return <div>Tabs</div>
+export type Tab = Omit<
+  ComponentPropsWithoutRef<typeof RadixTabs.Trigger>,
+  'children' | 'className' | 'key' | 'onClick'
+>
+
+type Props = {
+  getTabNameOnClick: (tabName: string) => void
+  selected?: string
+  tabs: Tab[]
+}
+
+export const Tabs = ({ getTabNameOnClick, selected, tabs }: Props) => {
+  const mappedData = tabs.map(({ value, ...rest }) => (
+    <RadixTabs.Trigger
+      className={clsx(s.tab, {
+        [s.selected]: value === selected,
+      })}
+      key={value}
+      onClick={() => getTabNameOnClick(value)}
+      value={value}
+      {...rest}
+    >
+      {value}
+    </RadixTabs.Trigger>
+  ))
+
+  return (
+    <RadixTabs.Root defaultValue={'account'}>
+      <RadixTabs.List>{mappedData}</RadixTabs.List>
+    </RadixTabs.Root>
+  )
 }
