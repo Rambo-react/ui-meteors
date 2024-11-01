85f2c28ccd32face1ce985b05dbcf82eb6e560ce
Yan Turnt <bx6xbchosen1@gmail.com>
2024-11-01 16:23:01 +0500

    PROJ-5_pagination feat: add pagination

--- src/components/Pagination/Pagination.stories.tsx
+++ src/components/Pagination/Pagination.stories.tsx
@@ -1,10 +1,38 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { Pagination } from './'
+import { useEffect, useState } from 'react'
+
+import { ItemsPerPage, Pagination } from './Pagination'
 
 const meta = {
-  argTypes: {},
-  args: {},
+  argTypes: {
+    currentPage: {
+      description: 'Allows you to select current page',
+    },
+    itemsCount: {
+      description: 'Allows you to set total number of items',
+    },
+    itemsPerPage: {
+      description: 'Allows you to select items per page',
+    },
+    onItemsPerPageChange: {
+      description: 'Callback function to get items per page after it has changed',
+    },
+    onPageChange: {
+      description: 'Callback function to get current page after it has changed',
+    },
+  },
+  args: {
+    currentPage: 3,
+    itemsCount: 510,
+    itemsPerPage: 20,
+    onItemsPerPageChange(itemsPerPage: ItemsPerPage) {
+      alert(itemsPerPage)
+    },
+    onPageChange(page: number) {
+      alert(page)
+    },
+  },
   component: Pagination,
   tags: ['autodocs'],
   title: 'Components/Pagination',
@@ -14,4 +42,24 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 // stories
-export const BasicExample: Story = {}
+export const BasicExample: Story = {
+  render(args) {
+    const [page, setPage] = useState(args.currentPage)
+    const [itemsPerPage, setItemsPerPage] = useState<ItemsPerPage>(args.itemsPerPage)
+
+    useEffect(() => {
+      setPage(args.currentPage)
+      setItemsPerPage(args.itemsPerPage)
+    }, [args.currentPage, args.itemsPerPage])
+
+    return (
+      <Pagination
+        {...args}
+        currentPage={page}
+        itemsPerPage={itemsPerPage}
+        onItemsPerPageChange={setItemsPerPage}
+        onPageChange={setPage}
+      />
+    )
+  },
+}
