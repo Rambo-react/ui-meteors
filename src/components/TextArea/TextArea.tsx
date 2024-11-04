c879737a52abb21500a0eb7638f437af818b1ac2
Rambo <sandstorm0819@gmail.com>
2024-11-04 22:57:09 +0300

    textAreaFix refactor: remove onChange, placeholder, value properties for textArea replace them rest operator

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -10,9 +10,6 @@ type Props = {
   error?: boolean
   errorText?: string
   label?: string
-  onChange?: (text: string) => void
-  placeholder?: string
-  value?: string
 } & ComponentPropsWithoutRef<'textarea'>
 
 export const TextArea = ({
@@ -21,9 +18,6 @@ export const TextArea = ({
   error,
   errorText = '',
   label = 'Text-area',
-  onChange,
-  placeholder = 'Text-area',
-  value,
   ...rest
 }: Props) => {
   const classNames = clsx(s.textarea, error && s.error, className)
@@ -37,16 +31,7 @@ export const TextArea = ({
           {label}
         </Label.Root>
       )}
-      <textarea
-        aria-label={label}
-        className={classNames}
-        id={id}
-        onChange={value => onChange?.(value)}
-        placeholder={placeholder}
-        value={value}
-        {...rest}
-        disabled={disabled}
-      />
+      <textarea aria-label={label} className={classNames} id={id} {...rest} disabled={disabled} />
       {errorText && <p className={s.errorText}>{errorText}</p>}
     </div>
   )
