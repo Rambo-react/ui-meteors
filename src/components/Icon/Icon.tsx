e4840490f872d9ddc68e1191db946cb8e67781e4
Yan Turnt <110596776+bx6xb@users.noreply.github.com>
2024-11-05 17:03:20 +0600

    iconFix refactor: remove useless props

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -3,22 +3,13 @@ import { ComponentPropsWithoutRef } from 'react'
 import sprite from './icons/sprite.svg'
 
 type Props = {
-  color?: string
-  height?: number
   id: string
-  stroke?: string
-  viewBox?: string
-  width?: number
 } & ComponentPropsWithoutRef<'svg'>
 
-export const Icon = ({ color, height, id, stroke, viewBox, width, ...rest }: Props) => {
+export const Icon = ({ id, viewBox, ...rest }: Props) => {
   return (
     <svg
-      fill={color}
-      height={height}
-      stroke={stroke}
       viewBox={viewBox || `0 0 24 24`}
-      width={width}
       xmlns={'http://www.w3.org/2000/svg'}
       {...rest}
     >
