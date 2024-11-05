e2c9e618cc7082fb4d7e69bfb2accf9bb9f83a85
Mary-No <mascha.novik@gmail.com>
2024-11-05 17:12:42 +0100

    PROJ-18_card fix: change props

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -3,19 +3,8 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Card } from '.'
 
 const meta = {
-  argTypes: {
-    content: {
-      cardTitle: {
-        type: 'string',
-      },
-      description: 'Variable card content',
-      sectionTitle: {
-        type: 'string',
-      },
-      text: {
-        type: 'string',
-      },
-    },
+  args: {
+    children: <div>Card</div>,
   },
   component: Card,
   tags: ['autodocs'],
@@ -25,12 +14,4 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {
-  args: {
-    content: {
-      cardTitle: 'Apple iMac 27',
-      sectionTitle: 'Active sessions',
-      text: 'Lorem ipsum odor amet, consectetuer adipiscing elit. Pretium montes rutrum; euismod ultricies pellentesque mus ut euismod.',
-    },
-  },
-}
+export const Default: Story = {}
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,21 +1,9 @@
 import styles from './Card.module.scss'
 
-type ContentProps = {
-  cardTitle?: string
-  sectionTitle?: string
-  text?: string
-}
 type CardProps = {
-  content: ContentProps
+  children: React.ReactNode
 }
-export const Card = ({ content }: CardProps) => {
-  return (
-    <div className={styles.cardWrapper}>
-      {content.sectionTitle ? <h3 className={styles.titleSection}>{content.sectionTitle}</h3> : ''}
-      <div className={styles.card}>
-        {content.cardTitle ? <p className={styles.cardTitle}>{content.cardTitle}</p> : ''}
-        {content.text ? <p className={styles.text}>{content.text}</p> : ''}
-      </div>
-    </div>
-  )
+
+export const Card = ({ children }: CardProps) => {
+  return <div className={styles.card}>{children}</div>
 }
