861dc38aaf6e8eacb0011be9eda4250d68f6f33e
krylov72 <qobject1337@gmail.com>
2024-11-05 20:58:23 +0500

    PROJ-3_modal feat:  added all possible variants of modal windows to stories

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -1,45 +1,69 @@
 import { Meta, StoryObj } from '@storybook/react'
+import s from '../Card/Card.module.scss'
 
 import { Modal } from './Modal'
+import { Icon, Input, Select, SelectItem } from '..'
 
 const meta = {
   component: Modal,
+  tags: ['autodocs'],
   title: 'Components/Modal',
   argTypes: {
     children: {
       control: 'select',
-      options: [
-        'Are you sure you want to delete this post?',
-        'We have sent a link to confirm your email to epam@epam.com',
-      ],
+      options: {
+        text: 'We have sent a link to confirm your email to epam@epam.com',
+        withInput: (
+          <div>
+            <p>Some words:</p>
+            <Input />
+          </div>
+        ),
+        withCard: (
+          <div
+            className={s.card}
+            style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+          >
+            <Icon id="image-outline" width={40} height={40} viewBox="0 0 30 30" />
+          </div>
+        ),
+      },
       description: 'Accepts whole components, which is very convenient in terms of scalability',
     },
     buttons: {
       control: 'select',
-      options: [['OK'], ['Yes', 'No']],
+      options: [['Button'], ['Button1', 'Button2'], ['Select from Computer', 'Open Draft']],
       description: `Accepts values in an array
 ['btn1','btn2',.....]`,
     },
+    firstBtnHandler: {},
   },
-}
+} satisfies Meta<typeof Modal>
 
 export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Example: Story = {
+export const SentToEmail: Story = {
   args: {
-    title: 'Title here',
+    title: 'Email sent',
+    children: <p>We have sent a link to confirm your email to epam@epam.com</p>,
+    buttons: ['OK'],
+  },
+}
+
+export const WithSelector: Story = {
+  args: {
+    title: 'Ban user',
     children: (
       <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <p>Hi</p>
-        <input type="text" name="" id="" />
-        <p>Hi</p>
-        <input type="text" name="" id="" />
-        <p>Hi</p>
-        <input type="text" name="" id="" />
+        <Select placeholder="Reason for ban">
+          <SelectItem value={'Bad behavior'}>Bad behavior</SelectItem>
+          <SelectItem value={'Advertising placement'}>Advertising placement</SelectItem>
+          <SelectItem value={'Another reason'}>Another reason</SelectItem>
+        </Select>
       </div>
     ),
-    buttons: ['Handler', 'New'],
+    buttons: ['No', 'Yes'],
   },
 }
