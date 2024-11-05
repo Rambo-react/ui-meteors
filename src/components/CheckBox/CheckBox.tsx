c7c8106d4e13aab31369f45f5e51e7c0ef481716
Rambo <sandstorm0819@gmail.com>
2024-11-05 17:39:20 +0300

    checkBoxRefactor feat: add types to props and remove unusless props

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -1,3 +1,5 @@
+import { ComponentPropsWithoutRef } from 'react'
+
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
 import clsx from 'clsx'
 
@@ -6,23 +8,15 @@ import s from './CheckBox.module.scss'
 import { Icon } from '../Icon'
 
 type Props = {
-  checked?: boolean
-  disabled?: boolean
   label?: string
-  onCheckedChange?: (checked: boolean) => void
-}
+} & ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
 
-export const CheckBox = ({ checked = false, disabled = false, label, onCheckedChange }: Props) => {
+export const CheckBox = ({ disabled = false, label, ...rest }: Props) => {
   return (
     <div className={s.container}>
       <label className={clsx(s.label, disabled && s.disabled)}>
         <div className={clsx(s.buttonWrapper, disabled && s.disabled)}>
-          <CheckboxPrimitive.Root
-            checked={checked}
-            className={s.checkbox}
-            disabled={disabled}
-            onCheckedChange={value => onCheckedChange?.(!!value)}
-          >
+          <CheckboxPrimitive.Root className={s.checkbox} disabled={disabled} {...rest}>
             <CheckboxPrimitive.Indicator className={clsx(s.indicator, disabled && s.disabled)}>
               <Icon height={14} id={'checkmark-outline'} viewBox={'4 8 14 14'} width={14} />
             </CheckboxPrimitive.Indicator>
