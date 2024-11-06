e3f8b23d498c67aa432bb80724dc49c1a50a27a4
krylov72 <qobject1337@gmail.com>
2024-11-06 18:18:39 +0500

    PROJ-3 modal fix: change structure of Component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,46 +1,25 @@
 import { ReactNode } from 'react'
-import { Button, Icon } from '..'
+
 import s from './Modal.module.scss'
-import clsx from 'clsx'
+
+import { Icon } from '..'
 
 type Props = {
-  title: string
-  buttons: string[]
   children?: ReactNode
-  firstBtnHandler: () => void
-  secondBtnHandler?: () => void
+  onCloseBtn: () => void
+  title: string
 }
 
-export const Modal = ({ buttons, title, children, firstBtnHandler, secondBtnHandler }: Props) => {
+export const Modal = ({ children, onCloseBtn, title }: Props) => {
   return (
     <div className={s.container}>
       <div className={s.head}>
         <p>{title}</p>
-        <button>
-          <Icon id="close" width={25} height={25} fill="white" />
+        <button onClick={onCloseBtn}>
+          <Icon fill={'white'} height={25} id={'close'} width={25} />
         </button>
       </div>
-      <div className={s.main}>
-        {children}
-        <div
-          className={clsx(s.btnContainer, {
-            [s.oneButton]: buttons.length === 1,
-            [s.moreButtons]: buttons.length > 1,
-          })}
-        >
-          {buttons.map((button, index) =>
-            index >= 1 ? (
-              <Button onClick={secondBtnHandler} variant="outline">
-                {button}
-              </Button>
-            ) : (
-              <Button onClick={firstBtnHandler} variant="primary">
-                {button}
-              </Button>
-            )
-          )}
-        </div>
-      </div>
+      <div className={s.main}>{children}</div>
     </div>
   )
 }
