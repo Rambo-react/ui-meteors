aea2a5367c62373d1e26c5d9febe6937a29634cb
krylov72 <qobject1337@gmail.com>
2024-11-13 11:25:15 +0500

    PROJ-3_modal fix: all functionality of Modal component has been replaced

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,25 +1,33 @@
 import { ReactNode } from 'react'
 
+import * as Dialog from '@radix-ui/react-dialog'
+
 import s from './Modal.module.scss'
 
 import { Icon } from '..'
 
 type Props = {
-  children?: ReactNode
+  children: ReactNode
+  isOpen: boolean
   onCloseBtn: () => void
   title: string
 }
 
-export const Modal = ({ children, onCloseBtn, title }: Props) => {
+export const Modal = ({ children, isOpen, onCloseBtn, title }: Props) => {
   return (
-    <div className={s.container}>
-      <div className={s.head}>
-        <p>{title}</p>
-        <button onClick={onCloseBtn}>
-          <Icon fill={'white'} height={25} id={'close'} width={25} />
-        </button>
-      </div>
-      <div className={s.main}>{children}</div>
-    </div>
+    <Dialog.Root open={isOpen}>
+      <Dialog.Portal>
+        <Dialog.Overlay className={s.overlay} />
+        <Dialog.Content className={s.container}>
+          <div className={s.headContainer}>
+            <Dialog.Title className={s.title}>{title}</Dialog.Title>
+            <Dialog.Close className={s.closeBtn} onClick={onCloseBtn}>
+              <Icon fill={'white'} height={25} id={'close-outline'} width={25} />
+            </Dialog.Close>
+          </div>
+          <div className={s.mainContainer}>{children}</div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
