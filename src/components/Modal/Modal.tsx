332c833b3f4a06c0dd240ed270d7241b6f0bfe78
Rambo <sandstorm0819@gmail.com>
2024-12-07 19:06:54 +0300

    PROJ-3_modal fix: styles for modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
 
 import * as Dialog from '@radix-ui/react-dialog'
+import clsx from 'clsx'
 
 import s from './Modal.module.scss'
 
@@ -8,24 +9,25 @@ import { CloseOutline } from '../Icons'
 
 export type ModalProps = {
   children: ReactNode
+  className?: string | undefined
   isOpen: boolean
   onClose: () => void
   title: string
 }
 
-export const Modal = ({ children, isOpen, onClose, title }: ModalProps) => {
+export const Modal = ({ children, className, isOpen, onClose, title }: ModalProps) => {
   return (
     <Dialog.Root open={isOpen}>
       <Dialog.Portal>
         <Dialog.Overlay aria-describedby={undefined} className={s.overlay} />
-        <Dialog.Content aria-describedby={''} className={s.container}>
+        <Dialog.Content aria-describedby={''} className={clsx(s.container, className)}>
           <div className={s.headContainer}>
             <Dialog.Title className={s.title}>{title}</Dialog.Title>
             <Dialog.Close className={s.closeBtn} onClick={onClose}>
-              <CloseOutline fill={'white'} height={25} width={25} />
+              <CloseOutline fill={'white'} height={28} width={28} />
             </Dialog.Close>
           </div>
-          <div className={s.mainContainer}>{children}</div>
+          {children}
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
