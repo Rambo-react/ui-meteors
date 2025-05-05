50160be94b1fd4d961abb86394c27e168906ef46
Veritas93 <loli93@yandex.ru>
2025-05-05 23:28:23 +0300

    Revert "Merge pull request #272 from Rambo-react/PROJ-25_datePicker"
    This reverts commit df03be07aad28d51269819d3b96bed938b5899c2, reversing
    changes made to 92680ac2aa153abbbc91542f259be06ec0e573dc.

--- src/components/DatePicker/Calendar/Calendar.tsx
+++ src/components/DatePicker/Calendar/Calendar.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useEffect } from 'react'
+import { forwardRef } from 'react'
 
 import clsx from 'clsx'
 
@@ -11,39 +11,15 @@ import { CalendarDay } from './CalendarDay'
 import { useCalendar } from './useCalendar'
 
 type Props = {
-  currentDate?: Date | null
   label?: string
   onCalendarBlur: () => void
   onDateSelect: (dateInMs: number) => void
-  onMonthChange: (date: Date) => void
   selectedDates: number[]
 }
 
 export const Calendar = forwardRef<HTMLDivElement, Props>(
-  ({ currentDate, label, onCalendarBlur, onDateSelect, onMonthChange, selectedDates }, ref) => {
-    const { selectedMonth, selectedYear, setSelectedMonth, setSelectedYear } = useCalendar(
-      currentDate || undefined
-    )
-
-    const handleMonthChange = (next: boolean) => {
-      const newDate = new Date(selectedYear, selectedMonth + (next ? 1 : -1), 1)
-
-      setSelectedMonth(newDate.getMonth())
-      setSelectedYear(newDate.getFullYear())
-      onMonthChange(newDate)
-    }
-
-    useEffect(() => {
-      if (currentDate) {
-        const isSameMonthYear =
-          currentDate.getMonth() === selectedMonth && currentDate.getFullYear() === selectedYear
-
-        if (!isSameMonthYear) {
-          setSelectedMonth(currentDate.getMonth())
-          setSelectedYear(currentDate.getFullYear())
-        }
-      }
-    }, [currentDate, selectedMonth, selectedYear, setSelectedMonth, setSelectedYear])
+  ({ label, onCalendarBlur, onDateSelect, selectedDates }, ref) => {
+    const { goToMonth, selectedMonth, selectedYear } = useCalendar()
 
     const mappedWeekDays = WEEK_DAYS.map(day => <div key={day}>{day}</div>)
 
@@ -71,7 +47,7 @@ export const Calendar = forwardRef<HTMLDivElement, Props>(
           <span>{`${MONTHS_NUMBER[selectedMonth]} ${selectedYear}`}</span>
 
           <div>
-            <button onClick={() => handleMonthChange(false)} onMouseDown={e => e.preventDefault()}>
+            <button onClick={() => goToMonth(true)} onMouseDown={e => e.preventDefault()}>
               <ArrowIosBack
                 fill={'var(--color-light-100)'}
                 height={20}
@@ -79,7 +55,7 @@ export const Calendar = forwardRef<HTMLDivElement, Props>(
                 width={20}
               />
             </button>
-            <button onClick={() => handleMonthChange(true)} onMouseDown={e => e.preventDefault()}>
+            <button onClick={() => goToMonth(false)} onMouseDown={e => e.preventDefault()}>
               <ArrowIosForward
                 fill={'var(--color-light-100)'}
                 height={20}
--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,22 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react'
-import InputMask from 'react-input-mask'
-
 import clsx from 'clsx'
 
 import s from './DatePicker.module.scss'
 
-import { Button } from '../Button'
 import { CalendarIcon, CalendarIconOutline } from '../Icons'
-import { Input } from '../Input'
 import { Calendar } from './Calendar'
-import {
-  addSelectedDateAC,
-  clearSelectedDatesAC,
-  toggleIsCalendarOpenAC,
-} from './datePickerReducer'
+import { addSelectedDateAC, toggleIsCalendarOpenAC } from './datePickerReducer'
 import { useDatePicker } from './useDatePicker'
-import { formatDate, formatSelectedDates, parseDateString } from './utils'
-import { TODAY } from './variables'
+import { formatSelectedDates } from './utils'
 
 type Props = {
   disabled?: boolean
@@ -27,30 +17,21 @@ type Props = {
 }
 
 export const DatePicker = ({ disabled, error, getDate, isRangeInput = false, label }: Props) => {
-  const [calendarDate, setCalendarDate] = useState<Date>(TODAY)
   const { calendarRef, dispatch, isCalendarOpen, onCalendarBlur, selectedDates } = useDatePicker()
-  const [localValue, setLocalValue] = useState('')
-  const inputRef = useRef<HTMLInputElement>(null)
-
-  useEffect(() => {
-    if (!isRangeInput && selectedDates.length > 0) {
-      const date = new Date(selectedDates[0])
-
-      setLocalValue(formatDate(date))
-      setCalendarDate(date)
-    } else if (!isRangeInput && selectedDates.length === 0) {
-      setLocalValue('')
-      setCalendarDate(TODAY)
-    }
-  }, [selectedDates, isRangeInput])
 
   const onDateSelect = (dateInMs: number) => {
     dispatch(addSelectedDateAC(dateInMs, isRangeInput))
-    const newDates = isRangeInput
-      ? [...selectedDates, dateInMs].sort().map(ms => new Date(ms))
-      : [new Date(dateInMs)]
 
-    getDate(newDates)
+    if (isRangeInput) {
+      if (selectedDates.length === 2) {
+        getDate([new Date(dateInMs)])
+      } else {
+        getDate([...selectedDates, dateInMs].sort().map(ms => new Date(ms)))
+      }
+    } else {
+      getDate(new Date(dateInMs))
+      dispatch(toggleIsCalendarOpenAC())
+    }
   }
 
   const toggleCalendar = () => {
@@ -58,41 +39,6 @@ export const DatePicker = ({ disabled, error, getDate, isRangeInput = false, lab
       dispatch(toggleIsCalendarOpenAC())
 
       setTimeout(() => calendarRef.current && calendarRef.current.focus(), 0)
-    }
-  }
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = e.target.value
-
-    setLocalValue(inputValue)
-
-    const date = parseDateString(inputValue)
-
-    if (date) {
-      setCalendarDate(date)
-      getDate([date])
-    } else {
-      setCalendarDate(TODAY)
-      getDate([])
-    }
-  }
-
-  const handleCalendarMonthChange = (newDate: Date) => {
-    setCalendarDate(newDate)
-  }
-
-  const handlerBlur = () => {
-    if (isRangeInput) {
-      return
-    }
-    const date = parseDateString(localValue)
-
-    if (date) {
-      dispatch(addSelectedDateAC(date.getTime(), false))
-      setLocalValue(formatDate(date))
-    } else {
-      setLocalValue('')
-      dispatch(clearSelectedDatesAC())
     }
   }
 
@@ -105,83 +51,34 @@ export const DatePicker = ({ disabled, error, getDate, isRangeInput = false, lab
     >
       {label && <span>{label}</span>}
 
-      {!isRangeInput ? (
-        <div
-          className={clsx(s.inputContainer, {
-            [s.calendarExpanded]: isCalendarOpen, // for background while calendar opened
-          })}
-        >
-          <InputMask
-            disabled={disabled}
-            mask={'99.99.9999'}
-            maskPlaceholder={null}
-            onBlur={handlerBlur}
-            onChange={handleInputChange}
-            value={localValue}
-          >
-            {props => (
-              <Input
-                {...props}
-                className={clsx(s.input, { [s.error]: error })}
-                disabled={disabled}
-                placeholder={'дд.мм.гггг'}
-                ref={inputRef}
-              />
-            )}
-          </InputMask>
-          <Button
-            className={s.calendarDate}
-            disabled={disabled}
-            onClick={toggleCalendar}
-            type={'button'}
-            variant={'text'}
-          >
-            {isCalendarOpen ? (
-              <CalendarIcon
-                fill={error ? 'var(--color-danger-500)' : 'var(--color-light-100)'}
-                height={24}
-                width={24}
-              />
-            ) : (
-              <CalendarIconOutline
-                fill={error ? 'var(--color-danger-500)' : 'var(--color-light-100)'}
-                height={24}
-                width={24}
-              />
-            )}
-          </Button>
-        </div>
-      ) : (
-        <div
-          className={clsx(s.dateDisplayer, {
-            [s.calendarExpanded]: isCalendarOpen, // for background while calendar opened
-          })}
-          onMouseDown={toggleCalendar}
-        >
-          {formatSelectedDates(selectedDates, isRangeInput)}
+      <div
+        className={clsx(s.dateDisplayer, {
+          [s.calendarExpanded]: isCalendarOpen, // for background while calendar opened
+        })}
+        onMouseDown={toggleCalendar}
+      >
+        {formatSelectedDates(selectedDates, isRangeInput)}
 
-          {isCalendarOpen ? (
-            <CalendarIcon
-              fill={error ? 'var(--color-danger-500)' : 'var(--color-light-100)'}
-              height={24}
-              width={24}
-            />
-          ) : (
-            <CalendarIconOutline
-              fill={error ? 'var(--color-danger-500)' : 'var(--color-light-100)'}
-              height={24}
-              width={24}
-            />
-          )}
-        </div>
-      )}
+        {isCalendarOpen ? (
+          <CalendarIcon
+            fill={error ? 'var(--color-danger-500)' : 'var(--color-light-100)'}
+            height={24}
+            width={24}
+          />
+        ) : (
+          <CalendarIconOutline
+            fill={error ? 'var(--color-danger-500)' : 'var(--color-light-100)'}
+            height={24}
+            width={24}
+          />
+        )}
+      </div>
+
       {isCalendarOpen && (
         <Calendar
-          currentDate={calendarDate}
           label={label}
           onCalendarBlur={onCalendarBlur}
           onDateSelect={onDateSelect}
-          onMonthChange={handleCalendarMonthChange}
           ref={calendarRef}
           selectedDates={selectedDates}
         />
