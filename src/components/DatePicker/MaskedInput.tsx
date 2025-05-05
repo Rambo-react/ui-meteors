8324f84961ad7bd5f304f4511bf55e00ad76d7ab
Veritas93 <loli93@yandex.ru>
2025-05-05 23:27:46 +0300

    Revert "Merge pull request #284 from Rambo-react/PROJ-25_datePicker"
    This reverts commit 99b5e2ee63165a838404e3d9c94fc86154479602, reversing
    changes made to 414200ad3649f675172c49f296218634de54c44c.

--- src/components/DatePicker/MaskedInput.tsx
+++ src/components/DatePicker/MaskedInput.tsx
@@ -12,7 +12,7 @@ type Props = {
   value?: string
 }
 
-export const MaskedInput = forwardRef<HTMLInputElement, Props>(props => {
+export const MaskedInput = forwardRef<HTMLInputElement, Props>((props, ref) => {
   const inputRef = useRef<HTMLInputElement | null>(null)
   const { ref: imaskRef } = useIMask({
     autofix: true,
